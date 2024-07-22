01a42b07237a84dab7861913460f531b49ab344a
Clicxl <hrishipuligal@gmail.com>
2024-07-22 19:49:51 +0530

    Added version Selector UI && Changed UI colors

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,13 @@
 import React, { useState } from "react";
 import Mod from "./Mod";
+import VersionSearch from "./VersionSearch";
+import { getData } from "../utils";
 
 export default function Main(props) {
   const [inputVal, setInputVal] = useState("");
   const [mods, setMods] = useState([]);
 
   const { url } = props;
-
-  async function getData(Baseurl, Name, limit = 10) {
-    const mod_ids = [];
-    const fullurl = Baseurl + "search?" + `query=${Name}` + `&limit=${limit}`;
-    try {
-      const response = await fetch(fullurl);
-      if (response.status !== 200) {
-        throw new Error(`Response failed ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      const mods = data.hits;
-      mods.forEach((mod) => {
-        // console.log(mod);
-        mod_ids.push({
-          ID: mod.project_id,
-          Title: mod.title,
-          Type: mod.categories,
-          Proj_Type: mod.project_type,
-          Version: mod.version,
-          Image: mod.icon_url,
-          Desc: mod.description,
-        });
-      });
-    } catch (err) {
-      console.log(err.message);
-    }
-
-    return mod_ids;
-  }
 
   async function searchFunction(e) {
     if (e.key === "Enter") {
@@ -51,14 +22,17 @@ export default function Main(props) {
 
   return (
     <div>
-      <input
-        type="text"
-        className="modInput"
-        placeholder="Search"
-        value={inputVal}
-        onChange={changeInputValue}
-        onKeyDown={searchFunction}
-      />
+      <div className="input">
+        <input
+          type="text"
+          className="modInput"
+          placeholder="Search"
+          value={inputVal}
+          onChange={changeInputValue}
+          onKeyDown={searchFunction}
+        />
+        <VersionSearch />
+      </div>
       <div className="mods">
         <Mod mods={mods} />
       </div>
